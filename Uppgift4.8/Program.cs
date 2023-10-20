98094a210546f889fccb0cbc448e12e2bee927bc
arun06001 <arun06001@E-PW0234KE>
2023-10-20 09:59:05 +0200

    Add project files.

---
+++ Uppgift4.8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Uppgift4_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in 2 strängar på varsin rad, den andra ska vara exakt 2 tecken");
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+
+            if (str1.Contains(str2[0]) && str1.Contains(str2[1]))
+            {
+                Console.WriteLine("Båda tecknen i den andra strängen förkommer i den första strängen.");
+            }
+            else
+            {
+                Console.WriteLine("Inga/bara ett av tecknen i den andra strängen förekommer i den första strängen");
+            }
+
+            Console.WriteLine("Tryck på valfri knapp för att avsluta");
+            Console.ReadKey();
+        }
+    }
+}
